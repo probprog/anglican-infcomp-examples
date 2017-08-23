3da57024e8dd0d6657cfe3f370c6ac0c1cf7685e
Will Harvey <willh@robots.ox.ac.uk>
2017-08-23 13:14:03 +0100

    some data

--- src/worksheets/state-space.clj
+++ src/worksheets/state-space.clj
@@ -19,6 +19,9 @@
             anglican.importance
             anglican.infcomp.core))
 ;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}
+;; <=
 
 ;; **
 ;;; Define the state space model:
@@ -47,25 +50,18 @@
 ;; **
 
 ;; @@
-(def test-file "3
-  4
-  1
-  5
-  2
-  1
-  3
-  3")
+(def dataset 1)
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;worksheets.state-space/test-file</span>","value":"#'worksheets.state-space/test-file"}
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;worksheets.state-space/dataset</span>","value":"#'worksheets.state-space/dataset"}
 ;; <=
 
 ;; @@
-(let [file test-file;(slurp "plots/state-space/model.csv")
-      file (str/split file #"\n")
+(let [file (slurp "plots/state-space/model.csv")
+      file (str/split file #",")
       file (map read-string file)
       
-      data "3,3,3,3,3,4";(slurp "plots/state-space/data_1.csv")
+      data (slurp (str "plots/state-space/data_" (str dataset) ".csv"))
       data (str/split data #",")
       data (map read-string data)]
       
@@ -85,18 +81,6 @@
 ;; <=
 
 ;; @@
-(defn combine-observes-fn [observes] (map :value observes))
-(def replier (zmq/start-replier state-space [observations init-dist trans-mult trans-offset trans-var obs-mult obs-offset obs-var] combine-observes-fn))
-;; @@
-;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;worksheets.state-space/replier</span>","value":"#'worksheets.state-space/replier"}
-;; <=
-
-;; @@
-(zmq/stop-replier replier)
-;; @@
-
-;; @@
 (defn print-inference-results [num-particles algorithm-name]
   (let [algorithm (case algorithm-name
                     "is" :importance
@@ -106,14 +90,14 @@
         inference-result-string (str/join
                                   (map (fn [state] (str/join "," (cons (:log-weight state) (:result state)))) states)
                                   "\n")]
-    (spit (str "plots/state-space/" algorithm-name "_1_" num-particles ".csv") inference-result-string)))
+    (spit (str "plots/state-space/" algorithm-name "_" dataset "_" num-particles ".csv") inference-result-string)))
 ;; @@
 ;; =>
 ;;; {"type":"html","content":"<span class='clj-var'>#&#x27;worksheets.state-space/print-inference-results</span>","value":"#'worksheets.state-space/print-inference-results"}
 ;; <=
 
 ;; @@
-(def particles-range [1 2 3 4 5])
+(def particles-range [10 100 1000 2000 3000])
 
 ;(map #(print-inference-results % "csis") particles-range)
 (map #(print-inference-results % "is") particles-range)
@@ -124,8 +108,26 @@
 ;; <=
 
 ;; @@
-(prior/sample-from-prior state-space [observations init-dist trans-mult trans-offset trans-var obs-mult obs-offset obs-var])
+(map :value (:samples (first (prior/sample-from-prior state-space [observations init-dist trans-mult trans-offset trans-var obs-mult obs-offset obs-var]))))
 ;; @@
+;; =>
+;;; {"type":"list-like","open":"<span class='clj-lazy-seq'>(</span>","close":"<span class='clj-lazy-seq'>)</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-double'>2.4944409506921863</span>","value":"2.4944409506921863"},{"type":"html","content":"<span class='clj-double'>10.766245486251762</span>","value":"10.766245486251762"},{"type":"html","content":"<span class='clj-double'>12.570233259239128</span>","value":"12.570233259239128"},{"type":"html","content":"<span class='clj-double'>15.793020818098146</span>","value":"15.793020818098146"},{"type":"html","content":"<span class='clj-double'>22.46609183869953</span>","value":"22.46609183869953"},{"type":"html","content":"<span class='clj-double'>23.521075918335004</span>","value":"23.521075918335004"}],"value":"(2.4944409506921863 10.766245486251762 12.570233259239128 15.793020818098146 22.46609183869953 23.521075918335004)"}
+;; <=
+
+;; @@
+(defn combine-observes-fn [observes] (map :value observes))
+(def replier (zmq/start-replier state-space [observations init-dist trans-mult trans-offset trans-var obs-mult obs-offset obs-var] combine-observes-fn))
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;worksheets.state-space/replier</span>","value":"#'worksheets.state-space/replier"}
+;; <=
+
+;; @@
+(zmq/stop-replier replier)
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-string'>&quot;ZMQ connection terminated.&quot;</span>","value":"\"ZMQ connection terminated.\""}
+;; <=
 
 ;; @@
 
