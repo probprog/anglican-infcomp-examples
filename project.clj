b38ac361cda76c9971757527866a8724806a4e52
Tuan-Anh Le <tuananh@robots.ox.ac.uk>
2017-08-29 12:00:43 +0100

    bump up dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5"]
                  [anglican "1.1.0-SNAPSHOT"]
-                 [anglican-infcomp "0.2.2-SNAPSHOT"]
+                 [anglican-infcomp "0.2.3-SNAPSHOT"]
                  [net.polyc0l0r/clj-hdf5 "0.2.2-SNAPSHOT"]
                  [rhizome "0.2.7"]
                  [org.clojure/data.priority-map "0.0.7"]]
